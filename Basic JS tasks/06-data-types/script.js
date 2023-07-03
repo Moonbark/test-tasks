46ff2626bd1265d5ac4c06e6033269e8a81a2691
Cedar Bark <128471482+Moonbark@users.noreply.github.com>
2023-07-03 17:48:09 +0300

    06-data-types
    Типы данных и их определение в JS с помощью унарного оператора typeof

---
+++ Basic JS tasks/06-data-types/script.js
@@ -0,0 +1,25 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+
+const varStr = 'This is a string';
+const varNum = Infinity;
+const varBool = false;
+const varNull = null;
+const varUndef = undefined;
+const varObj = {};
+const varArr = [];
+
+console.log( typeof varStr, typeof varNum, typeof varBool, typeof varNull, typeof varUndef, typeof varObj, typeof varArr);
